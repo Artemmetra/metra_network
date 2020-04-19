1e13d5d6572ffd25c25c2e007ddd0d46e5e34822
Artem Chinkarouk <artem.chinkarouk@live.com>
2020-04-19 19:52:05 -0400

    ELECTRON INIT

--- app.js
+++ app.js
@@ -1,2 +1,53 @@
-This is a test from ATOM IDE
-GLEB SAYS HI
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const ipc = electron.ipcMain;
+const fs = require('fs');
+
+const {
+  app,
+  BrowserWindow,
+  Menu
+} = electron;
+
+let mainWindow;
+
+  mainWindow = new BrowserWindow({  webPreferences: {nodeIntegration: true}, transparent: false, minWidth: 600, minHeight: 800, titleBarStyle: 'customButtonsOnHover', frame: false});
+  mainWindow.webContents.openDevTools()
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true,
+
+  }));
+
+
+  mainWindow.on('closed', ()=>{
+    mainWindow = null;
+  })
+
+  const mainMenu = Menu.buildFromTemplate(menuTemp);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+
+
+// TAKEN FROM NODE JS
+var inherits = function(ctor, superCtor) {
+  ctor.super_ = superCtor;
+  ctor.prototype = Object.create(superCtor.prototype, {
+    constructor: {
+      value: ctor,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
+};
+
+let crypto;
+try {
+  crypto = require('crypto');
+} catch (err) {
+  console.log('crypto support is disabled!');
+}
