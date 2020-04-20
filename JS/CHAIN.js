d7db6d149f5cef79316bfd7d4ee697d6f97eb47a
Artem Chinkarouk <artem.chinkarouk@live.com>
2020-04-19 21:13:00 -0400

    Chain started

--- JS/CHAIN.js
+++ JS/CHAIN.js
@@ -4,18 +4,20 @@ var SHA = require("crypto-js/SHA256");
 // Block - TEXT
 // HASH
 
-function new_block(transaction){
+let chain = [];
+
+function new_block(transaction,previous_hash){
   let block = {};
 
   block.data = transaction;
-  block.previous = //hash of the pervious block;
-  block.hash = "";
+  block.previous = previous_hash;
+  block.hash = SHA(JSON.stringify(transaction)).toString();
 
 
-
+  chain.push(block);
   return block;
 }
 
-let tr = [5,-2,-3];
+let transaction = [5,-2,-3];
 
 //SHA(file.name).toString();
