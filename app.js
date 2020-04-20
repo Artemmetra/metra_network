583e8ffcf59896c38bfa262ea298fefcdecf5153
Artem Chinkarouk <artem.chinkarouk@live.com>
2020-04-19 20:05:40 -0400

    8.05

--- app.js
+++ app.js
@@ -52,3 +52,14 @@ app.on('ready', function() {
   const mainMenu = Menu.buildFromTemplate(menuTemp);
   Menu.setApplicationMenu(mainMenu);
 });
+
+
+
+function load_file_JSON(path,qx){
+  if (fs.existsSync(path)) {
+    data = fs.readFileSync(path, 'utf8');
+    qx(JSON.parse(data));
+  }else{
+    qx(false);
+  }
+}
