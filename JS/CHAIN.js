02759f602ac300d1f367a3fed35c3e2b6f80d946
Artem Chinkarouk <artem.chinkarouk@live.com>
2020-04-19 20:59:58 -0400

    8.59

--- JS/CHAIN.js
+++ JS/CHAIN.js
@@ -1,1 +1,19 @@
 console.log("HELLO");
+
+
+// Block - TEXT
+// HASH
+
+function new_block(transaction){
+  let block = {};
+
+  block.data = transaction;
+  block.previous = //hash of the pervious block;
+  block.hash = "";
+
+
+
+  return block;
+}
+
+let tr = [5,-2,-3];
