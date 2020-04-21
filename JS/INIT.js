e28c3cee208833eed70f5513a9e35b108139cb17
Artem Chinkarouk <artem.chinkarouk@live.com>
2020-04-21 17:30:06 -0400

    21-04-2020 Plans for the day

--- JS/INIT.js
+++ JS/INIT.js
@@ -1,11 +1,13 @@
 
 let chain_view = ['div',{'id':'chain_view','class_add':['data_construct','grid']},[]];
 
+let transaction_bin = ['div',{'id':'transaction_bin','class_add':['data_construct','grid']},[]];
+
 let menu = [ button('ADD BLOCK','new_block([1,1,1]);show_chain()'), button('ADD TRANSACTION','transaction("sender", "receiver", "content", "time");console.log(bin.length)')]
 
 let header = ['header',{'id':'header','class_add':['grid']},menu];
 
-let home = ['div',{'id':'home','class_add':['grid']},[header,chain_view]];
+let home = ['div',{'id':'home','class_add':['grid']},[header,chain_view,transaction_bin]];
 
 document.body.appendChild(dom(home));
 
